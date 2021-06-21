029df2c6c15793d4ab8f08a2c0db1119d31b1db2
Miyaoke <48222230+Miyaoke@users.noreply.github.com>
2021-06-21 22:18:11 +0900

    change

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,22 +1,11 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(
-            function(cache){
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(
-        function (response) {
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
-        })
-    );
-});
+// service-worker.js
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+  });
+  
+  self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+  });
+  
+  // 現状では、この処理を書かないとService Workerが有効と判定されないようです
+  self.addEventListener('fetch', function(event) {});
